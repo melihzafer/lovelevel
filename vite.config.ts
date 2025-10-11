be238d77f191f84e18515dfcdbe4566a92bcc8d8
Melih Zafer Hyusein <62754311+melihzafer@users.noreply.github.com>
2025-10-11 21:42:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // GitHub Pages subpath
+  base: '/lovelevel', // GitHub Pages subpath
   plugins: [
     react(),
     VitePWA({
